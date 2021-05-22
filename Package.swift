589d747221753e0e4cbe34c019c4567feba7c367
Adrian Zimmermann <zadrian@web.de>
2021-05-22 11:30:34 +0200

    update to swift 5

--- Package.swift
+++ Package.swift
@@ -1,12 +1,15 @@
+// swift-tools-version:5.3
 import PackageDescription
 
 let package = Package(
     name: "Nifty",
-    dependencies: [
-        .Package(url: "https://github.com/nifty-swift/Nifty-libs.git", majorVersion: 1),
+    products: [
+        .library(name: "Nifty", targets: ["Nifty"])
     ],
-    exclude: [
-    	"Tests/KnownResults",
+    dependencies: [
+        .package(path: "../Nifty-libs"),
+    ],
+    targets: [
+        .target(name: "Nifty", dependencies: [.product(name: "CLapacke", package: "Nifty-libs"), .product(name: "CBlas", package: "Nifty-libs")])
     ]
-
 )
